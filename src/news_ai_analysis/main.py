bea9a1d3942163d74100c75a15a6f607bf86b59a
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-02-26 17:29:29 +0300

    infra: add an entry point to the app

--- src/news_ai_analysis/main.py
+++ src/news_ai_analysis/main.py
@@ -1,1 +1,18 @@
 # Точка входа в приложение fastapi
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="AI-анализ новостей",
+    version="1.0.0",
+    root_path="/api"
+)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
