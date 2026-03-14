4e88ec77d87ec53e77551ce74824fe0936e07988
Toster123 <mr.ilya.grigoriev@gmail.com>
2026-03-14 15:50:46 +0300

    feat: add google api embedder

--- src/news_ai_analysis/rag/service.py
+++ src/news_ai_analysis/rag/service.py
@@ -4,14 +4,27 @@ from .config import config
 from news_ai_analysis.config import config as global_config
 from .utils import *
 from sentence_transformers import SentenceTransformer
+from google import genai
+from google.genai import types
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 
 
+class GoogleEmbedder():
+    def __init__(self):
+        self.client = genai.Client()
+
+    def __call__(self, text):
+        return self.client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=text,
+        config=types.EmbedContentConfig(task_type="SEMANTIC_SIMILARITY")
+        ).embeddings
+
 class Vectorstore():
     def __init__(self, path: str = config.VECTOR_STORE_PATH, embedding_model: str = config.EMBEDDING_MODEL):
         self.emb_model = SentenceTransformer(
-            embedding_model)
+            embedding_model) if global_config.DISABLE_LOCAL_MODELS else GoogleEmbedder()
         self.__vectorstore = FAISS.load_local(
             os.getcwd() + path, self.emb_model, allow_dangerous_deserialization=True) if os.path.exists(os.getcwd() + path) else FAISS()
 
